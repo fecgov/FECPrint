b142819d13358f4006818f3cba35b8930fcb107a
Satheesh Panyala <satheesh.panyala@ctr.salientcrgt.com>
2015-05-01 14:19:48 +0000

    added this trunk folder accidentally during merge. deleting it

--- trunk/src/main/java/gov/fec/efo/fecprint/pdf/PDFConcatenateTaskListener.java
+++
@@ -1,9 +0,0 @@
-package gov.fec.efo.fecprint.pdf;
-
-
-public interface PDFConcatenateTaskListener {
-	
-	void concateCompleted(boolean status);	
-	
-	void concateProgress(int pageProcessedSoFar);
-}
--- trunk/src/main/java/gov/fec/efo/fecprint/pdf/SegregatedPagesTaskListener.java
+++
@@ -1,8 +0,0 @@
-package gov.fec.efo.fecprint.pdf;
-
-import org.apache.commons.lang.math.IntRange;
-
-public interface SegregatedPagesTaskListener {
-	
-	void taskCompleted(boolean status);
-}
